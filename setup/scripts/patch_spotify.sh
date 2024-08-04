66374eb3e44d9afe673cc4a3ea9117f191b55808
PhoDit <42234092+ThePhoDit@users.noreply.github.com>
2024-08-04 19:09:40 +0200

    Update patch_spotify.sh

--- setup/scripts/patch_spotify.sh
+++ setup/scripts/patch_spotify.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+cd "$HOME"
 git clone https://github.com/abba23/spotify-adblock.git
 cd spotify-adblock
 make
 mkdir -p ~/.spotify-adblock && cp target/release/libspotifyadblock.so ~/.spotify-adblock/spotify-adblock.so
 mkdir -p ~/.config/spotify-adblock && cp config.toml ~/.config/spotify-adblock
 flatpak override --user --filesystem="~/.spotify-adblock/spotify-adblock.so" --filesystem="~/.config/spotify-adblock/config.toml" com.spotify.Client
-cp desktop_entries/spotify.desktop "$HOME/.local/share/applications/com.spotify.Client.desktop"
-cd "$HOME"
+cp "$HOME/dotfiles/setup/desktop_entries/spotify.desktop" "$HOME/.local/share/applications/com.spotify.Client.desktop"
 rm -r spotify-adblock
+cd -
