c6beef3e1166fa9ab5f80d32b8a680b289061e11
ThePhoDit <42234092+ThePhoDit@users.noreply.github.com>
2024-09-05 21:17:26 +0200

    Install MultiMC

--- setup/scripts/desktop_utils_install.sh
+++ setup/scripts/desktop_utils_install.sh
@@ -12,3 +12,28 @@ flatpak install -y flathub com.core447.StreamController
 
 # OpenTabletDriver
 flatpak install -y flathub net.opentabletdriver.OpenTabletDriver
+
+# Install MultiMC
+PREV_DIR=$(pwd)
+
+cd "$HOME"
+wget https://files.multimc.org/downloads/mmc-develop-lin64.tar.gz
+tar -xzf mmc-develop-lin64.tar.gz
+
+mkdir "$HOME/Apps" > /dev/null 2>&1
+
+mv MultiMC "$HOME/Apps/MultiMC"
+
+rm mmc-develop-lin64.tar.gz
+
+cd "$HOME/Apps/MultiMC"
+
+wget https://multimc.org/images/infinity32.png
+
+cd "$HOME"
+
+cp dotfiles/setup/desktop_entries/multimc.desktop "$HOME/.local/share/applications/multimc.desktop"
+echo "Exec=$HOME/Apps/MultiMC/MultiMC" >> "$HOME/.local/share/applications/multimc.desktop"
+echo "Icon=$HOME/Apps/MultiMC/infinity32.png" >> "$HOME/.local/share/applications/multimc.desktop"
+
+cd PREV_DIR
